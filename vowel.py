17426c70d1e99fe56e5b05a34a6dbefe0e8fc9b8
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-01-30 03:35:04 -0500

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,9 +1,6 @@
-c = input("Please Enter Your Own Character : ")
-
-if(c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u' or c == 'A'
-       or c == 'E' or c == 'I' or c == 'O' or c == 'U'):
-    print("The Given Character ", c, "is a Vowel")
- elif:
-    print("The Given Character ", c, "is a Consonant")
- else:
- 	print("the given character",c," is invalid")
+a=['a','e','i','o','u','A','E','I','O','U'];
+s=str(input("Enter the letter:"))
+if(s in a):
+  print("vowel")
+else:
+print("consonant")
