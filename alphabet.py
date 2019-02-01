2c8257bbe848f3accd46c72946a1a971e43144b5
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-01-31 21:58:35 -0800

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,8 @@
+ip = input("Enter any character: ");
+if ip == '0':
+    exit();
+else:
+    if((ip>='a' and ip<='z') or (ip>='A' and ip<='Z')):
+    	print("Alphabet.");
+    else:
+    	print("No");
