c7381a03a4f8210757a38c396633c4a967b9692f
SNivethini <46355009+SNivethini@users.noreply.github.com>
2019-02-01 00:54:55 -0800

    Update and rename largenum.py to largenum.c

---
+++ largenum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int n1, n2, n3;
+
+    printf("Enter three different numbers: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    if( n1>=n2 && n1>=n3 )
+        printf("%d", n1);
+
+    if( n2>=n1 && n2>=n3 )
+        printf("%d", n2);
+
+    if( n3>=n1 && n3>=n2 )
+        printf("%d", n3);
+
+	return 0;
+}
